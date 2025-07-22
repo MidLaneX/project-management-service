974a7b76684d401226202fbbd0183816b0a8cbd2
Pasindu <pasindusathsara60@gmail.com>
2025-07-22 08:46:05 +0530

    Initial commit

---
+++ src/test/java/com/midlane/project_management_tool_project_service/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midlane.project_management_tool_project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
